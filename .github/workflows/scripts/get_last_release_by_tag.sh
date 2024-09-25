d1b6ae705d068a3f7affe6cadffabef58bc21688
nriofrio2508 <nicolasriofrio@yape.com.pe>
2024-09-25 01:38:16 -0500

    tags

--- .github/workflows/scripts/get_last_release_by_tag.sh
+++ .github/workflows/scripts/get_last_release_by_tag.sh
@@ -5,13 +5,10 @@ tags=$(git tag -l "$prefix_repo-v*" --sort=-taggerdate | grep -E "^$prefix_repo-
 
 if [ -n "$tags" ]; then
     for tag in $tags; do
-        echo "Antes respuesta"
         response_release=$(gh api -H "Accept: application/vnd.github.v3+json" "/repos/$repository_name/releases/tags/$tag" || true)
-        echo "Obtuvo respuesta"
         if echo "$response_release" | jq -e '.status' > /dev/null 2>&1; then
             continue
         fi
-        echo $tag
         filter_only_release=$(echo "$response_release" | jq 'select(.prerelease == false) | select(.draft == false) | .tag_name')
         if [ -n "$filter_only_release" ]; then
             echo "name=$filter_only_release" >> $GITHUB_OUTPUT

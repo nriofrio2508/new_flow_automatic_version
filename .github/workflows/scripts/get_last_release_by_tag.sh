3803fbdc35eda7ba9b0dbe1dbceace76f1fa83ed
nriofrio2508 <nicolasriofrio@yape.com.pe>
2024-09-25 01:08:00 -0500

    tags

--- .github/workflows/scripts/get_last_release_by_tag.sh
+++ .github/workflows/scripts/get_last_release_by_tag.sh
@@ -1,17 +1,18 @@
 repository_name=$1
-result_next_last_tag="${PREFIX_REPO}-v0.0.0"
-tags=$(git tag -l '${PREFIX_REPO}-v*' --sort=-taggerdate | grep -E "^${PREFIX_REPO}-v[0-9]+\.[0-9]+\.[0-9]+$" || true)
+prefix_repo=$2
+result_next_last_tag="$prefix_repo-v0.0.0"
+tags=$(git tag -l "$prefix_repo-v*" --sort=-taggerdate | grep -E "^$prefix_repo-v[0-9]+\.[0-9]+\.[0-9]+$" || true)
 
 if [ -n "$tags" ]; then
     for tag in $filtered_tags; do
         response_release=$(gh api -H "Accept: application/vnd.github.v3+json" "/repos/$repository_name/releases/tags/$tag" || true)
 
         if echo "$response_release" | jq -e '.status' > /dev/null 2>&1; then
-        continue
+            continue
         fi
         filter_only_release=$(echo "$response_release" | jq 'select(.prerelease == false) | select(.draft == false) | .tag_name')
         if [ -n "$filter_only_release" ]; then
-        echo "name=$filter_only_release" >> $GITHUB_OUTPUT
+            echo "name=$filter_only_release" >> $GITHUB_OUTPUT
         break
         fi
     done
